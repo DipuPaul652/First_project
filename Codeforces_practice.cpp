8783b8f4d654133f13eccb50739a7c950a313560
contest_solution <dipupaul652@gmail.com>
2026-03-20 22:26:34 +0600

    codeforces no 1624B

--- Codeforces_practice.cpp
+++ Codeforces_practice.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+/*#include<bits/stdc++.h>
 using namespace std;
 
 int main()
@@ -12,6 +12,38 @@ int main()
     }
     else{
         cout<<"You can not vote"<<endl;
+    }
+
+    return 0;
+}*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    while(t--){
+        int a,b,c;
+        cin>>a>>b>>c;
+
+        int found=0;
+        if((2*b-c)>0 && (2*b-c)%a==0){
+            found=1;
+        }
+        else if(((a+c)/2)%2==0 && ((a+c)/2)%b==0){
+            found=1;
+        }
+        else if((2*b-a)>0 && (2*b-a)%c==0){
+            found=1;
+        }
+
+        if(found)
+        cout<<"YES"<<endl;
+        else
+        cout<<"NO"<<endl;
     }
 
     return 0;
